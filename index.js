69f1f711410620a21f772aec6ef97848656c4144
andela-fomokaro <faith.omokaro@andela.com>
2018-08-05 18:28:00 +0100

    Parsing request paths ---> get values from url and log to console

--- index.js
+++ index.js
@@ -6,10 +6,22 @@
 
 // Dependencies
 import http from 'http';
+import url from 'url';
 
 // The server should respond to all requests with a string
 const server = http.createServer( (req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request received on path: '+trimmedPath);
 });
 
 //Start the server, and have it listen on port 3000
